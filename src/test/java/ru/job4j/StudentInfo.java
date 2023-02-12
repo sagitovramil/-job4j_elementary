56b67ef5a67a1ea46186d2e9a73b4338cf4b7830
sagitovramil <sagitovramil86@gmail.com>
2023-02-12 21:11:17 +0500

    Добавление класса на github [#243565]

---
+++ src/test/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Ramil Sagitov");
+        System.out.println("27.02.1986");
+    }
+}
