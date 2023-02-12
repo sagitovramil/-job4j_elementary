249bad052d6e1940bbcb8da277f4e9dcf0fcd225
sagitovramil <sagitovramil86@gmail.com>
2023-02-12 22:30:56 +0500

    Добавление класса Multiple на github [#243569]

--- src/test/java/ru/job4j/StudentInfo.java
+++ src/test/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
-public class StudentInfo {
-    public static void main(String[] args){
+public class StudentInfo{
+    public static void main(String[] args) {
         System.out.println("Ramil Sagitov");
         System.out.println("27.02.1986");
     }
